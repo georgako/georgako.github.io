632a15b75e65e6481051231e0d151d577ffa28b3
Anastasios Georgakopoulos <georgako@gmail.com>
2018-09-17 15:28:04 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Ενοικιαζόμενα δωμάτια Αντώνης - Επικοινωνήστε μαζί μας</title>
+<link href="css/sifnos.css" rel="stylesheet" type="text/css" />
+<link href="css/menu.css" rel="stylesheet" type="text/css" />
+<script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
+<script src='https://www.google.com/recaptcha/api.js?hl=el'></script>
+</head>
+
+<body>
+<!--[if lte IE 6]>
+	<script type="text/javascript" src="supersleight-min.js"></script>
+<![endif]-->
+
+<div id="container">
+<div id="topImg">
+<img src="images/exterior-spaces.jpg" width="540" height="250" alt="Εξωτερικοί χώροι στα δωμάτια Αντώνης στο Κάστρο" />
+</div>
+<div id="menu">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr valign="top">
+      <td class="menu"><a href="index.html">Αρχική</a></td>
+      </tr>
+    <tr valign="top">
+      <td class="menu"><a href="rooms.html">Τα δωμάτια</a></td>
+      </tr>
+    <tr valign="top">
+      <td class="menu"><a href="rates.html">Τιμές</a></td>
+      </tr>
+    <tr valign="top">
+      <td class="menu"><b>Επικοινωνία</b></td>
+      </tr>
+    <tr valign="top">
+      <td class="menu"><a href="location.html">Θέση</a></td>
+      </tr>
+    <tr valign="top">
+      <td class="menu"><a href="kastro.html">Το Κάστρο</a></td>
+      </tr>
+      <tr valign="middle">
+      <td align="center"><br />
+        <a href="en/index.html"><img src="images/flags/English.gif" width="16" height="11" alt="Rooms Antonis in Sifnos - English" /> </a> <a href="fr/index.html"><img src="images/flags/french.gif" width="16" height="11" alt="Chambres Antonis Sifnos - français" /> </a> <a href="index.html"><img src="images/flags/greek.gif" width="16" height="11" alt="Δωμάτια Αντώνης στη Σίφνο - Ελληνικά" /></a></td>
+      </tr>
+    </table>
+  </div>
+  <div id="mainContent">
+    <h1>Επικοινωνία &amp; Κρατήσεις</h1>
+    <table width="100%" summary="Στοιχεία επικοινωνίας με τα δωμάτια Αντώνης">
+      <tr>
+        <td width="39%"><p>Για περισσότερες πληροφορίες και κρατήσεις μπορείτε 
+        να επικοινωνήσετε μαζί μας τηλεφωνικά ή να μας στείλετε την φόρμα που ακολουθεί:</p></td>
+        <td width="31%"><div id="contact">
+          <p>Αντώνης Ατσόνιος<br />
+            Κάστρο - Σίφνος<br />
+            Κυκλάδες
+            </p>
+          </div>
+        </td>
+        <td width="30%"><div id="contact">
+          <p>Τηλ.: 22840 33708<br />
+            Κιν.: 6947 021 343<br />
+            <img src="images/zak.png" width="172" height="14" align="absmiddle" /></p>
+        </div>
+        </td>
+      </tr>
+    </table>
+    <form action="http://www.sifnosholidays.gr/HDWForm2Mail/Form2MailNoCaptcha.php" method="post" name="form1" id="form1" accept-charset="utf-8">
+      <fieldset>
+        <legend>Σίφνος - Δωμάτια Αντώνης</legend>
+        <span id="sprytextfield1">
+          <label for="name">Ονοματεπώνυμο:</label>
+          <input name="name" type="text" id="name" size="30" />
+          <span class="textfieldRequiredMsg">Παρακαλούμε πληκτρολογήστε το ονοματεπώνυμό σας</span><span class="textfieldMinCharsMsg">Παρακαλούμε πληκτρολογήστε το ονοματεπώνυμό σας</span></span> <br />
+        <span id="sprytextfield2">
+          <label for="email">Email:</label>
+          <input name="email" type="text" id="email" size="30" />
+          <span class="textfieldRequiredMsg">Παρακαλούμε πληκτρολογήστε το email σας</span><span class="textfieldInvalidFormatMsg">Invalid format.</span></span> <br />
+        <span id="sprytextfield3">
+          <label for="phone">Τηλέφωνο:</label>
+          <input name="phone" type="text" id="phone" size="30" />
+          <span class="textfieldRequiredMsg">Παρακαλούμε πληκτρολογήστε το τηλέφωνό σας</span><span class="textfieldMinCharsMsg">Minimum number of characters not met.</span></span> <br />
+        <input name="separator1" type="hidden" value="separator1" />
+        Έχετε ξαναεπισκευθεί τη μονάδα μας?
+  <input type="radio" name="revisit" id="yes" value="yes" />
+  <label for="yes">Ναι</label>
+  <input type="radio" name="revisit" id="no" value="no" />
+  <label for="no">Όχι</label>
+  <br />
+  <label for="wish">Θα ήθελα:</label>
+  <select name="wish" id="wish">
+    <option>παρακαλώ επιλέξτε</option>
+    <option value="availability">να ελέγξω τη διαθεσιμότητα</option>
+    <option value="information">να μάθω επιπλέον πληροφορίες</option>
+    <option value="reservation">να κάνω μία κράτηση</option>
+  </select>
+  <br />
+  <label for="roomType">Τύπος καταλύματος:</label>
+  <select name="roomType" id="roomType">
+    <option value="2 bed room">Δίκλινο δωμάτιο</option>
+    <option value="3 bed room">Τρίκλινο δωμάτιο</option>
+    <option value="AnyAvailable">Οτιδήποτε διαθέσιμο</option>
+  </select>
+  <br />
+  <label for="persons">Αριθμός ατόμων:</label>
+  <select name="persons" id="persons">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="group">Group</option>
+  </select>
+  <input name="separator2" type="hidden" value="separator2" />
+  <br />
+  <label for="arrival_day">Ημερομηνία άφιξης:</label>
+  <select name="arrival_day" id="arrival_day">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+    <option value="11">11</option>
+    <option value="12">12</option>
+    <option value="13">13</option>
+    <option value="14">14</option>
+    <option value="15">15</option>
+    <option value="16">16</option>
+    <option value="17">17</option>
+    <option value="18">18</option>
+    <option value="19">19</option>
+    <option value="20">20</option>
+    <option value="21">21</option>
+    <option value="22">22</option>
+    <option value="23">23</option>
+    <option value="24">24</option>
+    <option value="25">25</option>
+    <option value="26">26</option>
+    <option value="27">27</option>
+    <option value="28">28</option>
+    <option value="29">29</option>
+    <option value="30">30</option>
+    <option value="31">31</option>
+  </select>
+  <select name="arrival_month" id="arrival_month">
+    <option value="Ιανουαρίου">Ιανουαρίου</option>
+    <option value="Φεβρουαρίου">Φεβρουαρίου</option>
+    <option value="Μαρτίου">Μαρτίου</option>
+    <option value="Απριλίου">Απριλίου</option>
+    <option value="Μαΐου">Μαΐου</option>
+    <option value="Ιουνίου">Ιουνίου</option>
+    <option value="Ιουλίου">Ιουλίου</option>
+    <option value="Αυγούστου">Αυγούστου</option>
+    <option value="Σεπτεμβρίου">Σεπτεμβρίου</option>
+    <option value="Οκτωβρίου">Οκτωβρίου</option>
+    <option value="Νοεμβρίου">Νοεμβρίου</option>
+    <option value="Δεκεμβρίου">Δεκεμβρίου</option>
+  </select>
+  <br />
+  <label for="departure_day">Ημερομηνία αναχώρησης:</label>
+  <select name="departure_day" id="departure_day">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+    <option value="11">11</option>
+    <option value="12">12</option>
+    <option value="13">13</option>
+    <option value="14">14</option>
+    <option value="15">15</option>
+    <option value="16">16</option>
+    <option value="17">17</option>
+    <option value="18">18</option>
+    <option value="19">19</option>
+    <option value="20">20</option>
+    <option value="21">21</option>
+    <option value="22">22</option>
+    <option value="23">23</option>
+    <option value="24">24</option>
+    <option value="25">25</option>
+    <option value="26">26</option>
+    <option value="27">27</option>
+    <option value="28">28</option>
+    <option value="29">29</option>
+    <option value="30">30</option>
+    <option value="31">31</option>
+  </select>
+  <select name="departure_month" id="departure_month">
+    <option value="Ιανουαρίου">Ιανουαρίου</option>
+    <option value="Φεβρουαρίου">Φεβρουαρίου</option>
+    <option value="Μαρτίου">Μαρτίου</option>
+    <option value="Απριλίου">Απριλίου</option>
+    <option value="Μαΐου">Μαΐου</option>
+    <option value="Ιουνίου">Ιουνίου</option>
+    <option value="Ιουλίου">Ιουλίου</option>
+    <option value="Αυγούστου">Αυγούστου</option>
+    <option value="Σεπτεμβρίου">Σεπτεμβρίου</option>
+    <option value="Οκτωβρίου">Οκτωβρίου</option>
+    <option value="Νοεμβρίου">Νοεμβρίου</option>
+    <option value="Δεκεμβρίου">Δεκεμβρίου</option>
+  </select>
+  <br />
+  <label for="stays">Αριθμός διανυκτερεύσεων:</label>
+  <input name="stays" type="text" id="stays" size="5" />
+  <br />
+  <input name="separator3" type="hidden" value="separator3" />
+  <label for="comments">Σχόλια:</label>
+  <textarea name="comments" cols="40" rows="5" id="comments"></textarea>
+  <br />
+  <div class="captcha" align="right">
+                            <p style="text-align:right">Συμπληρώστε (υποχρεωτικό):</p>
+                            <div class="g-recaptcha" data-sitekey="6LfjTBcUAAAAAIUB3g0uuHuzPirlo_7231FbPL6A"></div>
+                            <script>
+                                          var onSuccess = function(response) {
+                                            var errorDivs = document.getElementsByClassName("recaptcha-error");
+                                            if (errorDivs.length) {
+                                              errorDivs[0].className = "";
+                                            }
+                                            var errorMsgs = document.getElementsByClassName("recaptcha-error-message");
+                                            if (errorMsgs.length) {
+                                              errorMsgs[0].parentNode.removeChild(errorMsgs[0]);
+                                            }
+                                          };
+                                        </script> 
+                            <!-- END: ReCAPTCHA --></div>
+   <input type="submit" name="submit" id="submit" value="Αποστολή φόρμας" />
+   <input type="reset" name="reset" id="reset" value="Καθαρισμός πεδίων" />
+  <br />
+      </fieldset>
+      <input type="hidden" name="hdwemail" id="hdwemail" value="" />
+      <input type="hidden" name="hdwok" id="hdwok" value="confirmation.html" />
+      <input type="hidden" name="hdwnook" id="hdwnook" value="error.html" />
+    </form>
+  </div>
+<div id="footer">
+&copy; Antonis Rooms 2011 | <a href="http://lysiteleia.gr" target="_blank" class="lysiteleia">Δημιουργία - Φιλοξενία: Λυσιτέλεια</a>
+</div>
+<!-- end #container --></div>
+<script type="text/javascript">
+<!--
+var sprytextfield1 = new Spry.Widget.ValidationTextField("sprytextfield1", "none", {validateOn:["blur"], minChars:3});
+var sprytextfield2 = new Spry.Widget.ValidationTextField("sprytextfield2", "email", {validateOn:["blur"]});
+var sprytextfield3 = new Spry.Widget.ValidationTextField("sprytextfield3", "none", {validateOn:["blur"], minChars:10});
+//-->
+</script>
+</body>
+</html>
